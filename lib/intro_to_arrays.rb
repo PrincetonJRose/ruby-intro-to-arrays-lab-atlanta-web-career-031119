769dfd68a87a058d2696a854cc450a73d66fdc8f
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 21:21:32 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+  hello = []
+end
+
+def array_with_two_elements
+  hello = [1,2]
+end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+
+  
